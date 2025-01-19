040f1c4a2fb10c6e7ce7e582a1b61b8a2b14357a
RajalakshmiShankar <laxmi.achu003@gmail.com>
2025-01-19 23:57:00 +0530

    investment_report

--- Investment Report/investment_report.py
+++ Investment Report/investment_report.py
@@ -1,10 +1,9 @@
 initial_investment = float(input("Enter the initial investment: "))
 annual_interest_rate= float(input("Enter the annual interest rate(in %):"))
 year=int (input("Enter the number of years:"))
-annual_interest_rate/=100
 
 print("\nYear\tAmount")
 amount=initial_investment
 for year in range(0,year):
-    amount+=amount*annual_interest_rate
-    print(year+1, "\t", "%.2f" % amount)
+    amount+=amount*(annual_interest_rate/100)
+    print(year+1, "\t", round(amount,2))
