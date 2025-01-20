a2ec13e76781ed6cf3f83388fa8b5853091f0f73
RajalakshmiShankar <laxmi.achu003@gmail.com>
2025-01-20 22:11:46 +0530

    income_tax

--- Income Tax Calculator/income_tax.py
+++ Income Tax Calculator/income_tax.py
@@ -1,10 +1,21 @@
-income=int(input("Enter Your Income :"))
-if income<=250000:
-    tax=0
-elif income > 250000 and income <= 500000:
-    tax=(income-250000)*0.05
-elif income>500000 and income <=1000000:
-    tax=(income-500000)*0.2 + 250000*0.05
+
+income=float(input("Enter Your Income :"))
+
+if income < 0:
+    print("Invalid income")
 else:
-    tax=(income-1000000)*0.3 +500000*0.2+250000*0.05
+    tax = 0
+    if income <= 300000:
+        tax = 0  
+    elif income <= 600000:
+        tax = (income - 300000) * 0.05  
+    elif income <= 900000:
+        tax = (300000 * 0.05) + (income - 600000) * 0.1  
+    elif income <= 1200000:
+        tax = (300000 * 0.05) + (300000 * 0.1) + (income - 900000) * 0.15  
+    elif income <= 1500000:
+        tax = (300000 * 0.05) + (300000 * 0.1) + (300000 * 0.15) + (income - 1200000) * 0.2  
+    else:
+        tax = (300000 * 0.05) + (300000 * 0.1) + (300000 * 0.15) + (300000 * 0.2) + (income - 1500000) * 0.3  
+
 print("The Income Tax for",income ,"is",tax)
